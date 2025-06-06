87b18fcdffb40d3d863fa98790b8dd0dff3ba421
Lone-Air <laman28-lmfs@outlook.com>
2025-06-07 03:29:55 +0800

    reborn

--- showshowway/__init__.py
+++ showshowway/__init__.py
@@ -1,4 +1,5 @@
 import re
+import nbtlib as snbt
 from mcdreforged.api.all import *
 
 version = '1.2.0'
@@ -6,47 +7,37 @@ version = '1.2.0'
 
 def on_load(server, params):
     server.logger.info('正在注册指令')
-    server.register_help_message('!!skull <player>', '获取指定玩家的头颅')
     server.register_help_message('!!show', '展示自己手上的物品')
 
 
 def on_info(server: ServerInterface, info: Info):
-    if info.is_player and info.content.find('!!skull ') > -1:
-        give_skull(server, info)
     if info.is_player and info.content == '!!show':
         player = info.player
         if server.is_rcon_running():
             data = server.rcon_query('data get entity {} SelectedItem'.format(player))
-            nbt = data.lstrip(re.search(r'\w+ has the following entity data: ', data).group()).replace('"', r'\"')
-            json = '[{"text":"[ShowIt] "},{"text":"' + player + '","color":"yellow"},{"text":" 正在展示一个物品！ "},' \
-                                                                '{"text":"[点我查看]","color":"aqua","bold":true,' \
-                                                                '"underlined":true,"hoverEvent":{' \
-                                                                '"action":"show_item","value":"' + nbt + '"}}]'
+            nbt = data.lstrip(re.search(r'\w+ has the following entity data: ', data).group())
+            nbtj = snbt.parse_nbt(nbt)
+            try:
+                nbtj.get("components").unpack()
+            except Exception:
+                nbtj.update({"components": snbt.Compound({})})
+            json = '[{"text":"[ShowIt] "},{"text":"%s","color":"yellow"},{"text":" 正在展示一个物品！ "},{"text":"[查看]","color":"aqua","bold":true,"underlined":true,"hover_event":{"action":"show_item","id":"%s","count":%d,"components":%s}}]' % (player, nbtj.get("id").unpack(), nbtj.get("count").unpack(), nbtj.get("components").unpack())
             show_item(server, json)
         else:
             server.execute('data get entity {} SelectedItem'.format(player))
     if not info.is_player and re.match(r'\w+ has the following entity data: ', info.content) is not None:
-        nbt = info.content.lstrip(re.search(r'\w+ has the following entity data: ', info.content).group()).replace('"',
-                                                                                                                   r'\"')
+        nbt = info.content.lstrip(re.search(r'\w+ has the following entity data: ', info.content).group())
         player = re.search(r'^\w+', info.content).group()
         if nbt[0] == '{':
-            json = '[{"text":"[ShowIt] "},{"text":"' + player + '","color":"yellow"},{"text":" 正在展示一个物品！ "},' \
-                                                                '{"text":"[点我查看]","color":"aqua","bold":true,' \
-                                                                '"underlined":true,"hoverEvent":{' \
-                                                                '"action":"show_item","value":"' + nbt + '"}}]'
+            nbtj = snbt.parse_nbt(nbt)
+            try:
+                nbtj.get("components").unpack()
+            except Exception:
+                nbtj.update({"components": snbt.Compound({})})
+            json = '[{"text":"[ShowIt] "},{"text":"%s","color":"yellow"},{"text":" 正在展示一个物品！ "},{"text":"[查看]","color":"aqua","bold":true,"underlined":true,"hover_event":{"action":"show_item","id":"%s","count":%d,"components":%s}}]' % (player, nbtj.get("id").unpack(), nbtj.get("count").unpack(), nbtj.get("components").unpack())
             show_item(server, json)
-
-
-def give_skull(server, info):
-    info.cancel_send_to_server()
-    owner = '[profile={name:"' + info.content[8:] + '"}]'
-    player = info.player
-    message = '[ShowIt] 已给予你 {} 的头颅'.format(info.content[8:])
-    server.logger.info('{} 获取 {} 的头颅'.format(player, info.content[8:]))
-    server.execute('execute at {0} run give {0} minecraft:player_head{1}'.format(player, owner))
-    server.reply(info, message)
 
 
 def show_item(server, json):
     server.execute('tellraw @a {}'.format(json))
-    server.execute('execute run playsound minecraft:entity.arrow.hit_player player @a')
+    server.execute('execute as @a at @s run playsound minecraft:entity.arrow.hit_player player @s')
